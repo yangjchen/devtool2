e53946a1fd901908b99636bfd114f44c81bb03e4
Jaco <yangjchen@qq.com>
2023-05-05 23:49:09 +0800

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -45,10 +45,10 @@ export default async function ProjectsPage() {
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
 					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						Projects
+						Dev Tool 2.0
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
+						建立开发者工具技术雷达持续观测更新，形成 Dev Tool 2.0 风向标
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
