26056c2e9e5eefab1b99da14a1cfa2b88b578fae
Jaco <yangjchen@qq.com>
2023-05-05 22:54:03 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Particles from "./components/particles";
 
 const navigation = [
 	{ name: "Dev Tool 2.0", href: "/projects" },
-	{ name: "Biang Design", href: "/contact" },
+	{ name: "Biang Design", href: "biang.design" },
 ];
 
 export default function Home() {
